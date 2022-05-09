763f989ff7a767cf129429bf38732e6b319eabce
JimJ92120 <57893611+JimJ92120@users.noreply.github.com>
2022-05-10 00:25:38 +0700

    feat(blocks > patterns): register theme category

--- functions.php
+++ functions.php
@@ -7,6 +7,16 @@ add_action('after_theme_setup', function () {
     add_theme_support('post-thumbnails');
     add_theme_support('align-wide');
 
+    add_theme_support('block-templates');
     add_theme_support('wp-block-styles');
     add_theme_support('editor-styles');
+});
+
+add_action('init', function () {
+    register_block_pattern_category(
+        'wordpress-block-theme',
+        [
+            'label' => __('Block Theme', 'wordpress-block-theme')
+        ]
+    );
 });
